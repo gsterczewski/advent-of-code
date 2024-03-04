bb494e5e1c7952e52a3c8bf9ff3ca3da136b9b09
Grzegorz Sterczewski <sterczewski.dev@protonmail.ch>
2024-03-04 15:45:14 +0100

    updated main.go

--- main.go
+++ main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"advent-of-code/solutions"
+	"advent-of-code/utils"
 	"fmt"
 )
 
 func main() {
-	fmt.Println(solutions.SolutionDay1())
-	fmt.Println(solutions.SolutionDay2())
-	fmt.Println(solutions.SolutionDay2B())
+	fmt.Printf("day 1 solution A : %d\n", solutions.SolutionDay1A(utils.GetInput("day1-input.txt")))
+	// fmt.Printf("day 1 solution B : %d\n", solutions.SolutionDay1B(utils.GetInput("day1-input.txt")))
+	fmt.Printf("day 2 solution A : %d\n", solutions.SolutionDay2A(utils.GetInput("day2-input.txt")))
+	fmt.Printf("day 2 solution B : %d\n", solutions.SolutionDay2B(utils.GetInput("day2-input.txt")))
 }
