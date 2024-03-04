19c48527ac27f8d716e2cb6da7c23ac32cc48c81
Grzegorz Sterczewski <sterczewski.dev@protonmail.ch>
2024-03-04 19:22:11 +0100

    add generic stack ds to utils

--- utils/utils.go
+++ utils/utils.go
@@ -19,3 +19,38 @@ func GetInput(inputPath string) *bufio.Scanner {
 	scanner.Split(bufio.ScanLines)
 	return scanner
 }
+
+type Stack[T any] struct {
+	internalStack []T
+	len           int
+}
+
+func (st Stack[T]) Size() int {
+	return st.len
+}
+func (st Stack[T]) Top() T {
+	if st.IsEmpty() {
+		return *new(T)
+	}
+	return st.internalStack[st.Size()-1]
+}
+func (st Stack[T]) IsEmpty() bool {
+	return len(st.internalStack) == 0
+}
+func (st *Stack[T]) Pop() T {
+	if st.IsEmpty() {
+		return *new(T)
+	}
+	lastElement := st.Top()
+	lastIndex := st.len - 1
+	st.internalStack = st.internalStack[:lastIndex]
+	st.len--
+	return lastElement
+}
+func (st *Stack[T]) Push(el T) {
+	st.internalStack = append(st.internalStack, el)
+	st.len++
+}
+func CreateStack[T any]() Stack[T] {
+	return Stack[T]{internalStack: make([]T, 0), len: 0}
+}
